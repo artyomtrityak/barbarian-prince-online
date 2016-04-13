0a52a7f7c05b3b935dc02031ef33e7c69640b364
Artyom Trityak <art.trityak@gmail.com>
2016-04-12 23:30:27 -0700

    Init get messages

--- index.js
+++ index.js
@@ -12,8 +12,24 @@ app.get('/', function(request, response) {
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_secret_webhook_change_me') {
     res.send(req.query['hub.challenge']);
+    return;
   }
   res.send('Error, wrong validation token');
+});
+
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      // Handle a text message from this sender
+      text = event.message.text;
+      console.log('TEXT:', text);
+    }
+  }
+  res.sendStatus(200);
 });
 
 
