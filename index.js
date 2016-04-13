abf135355e952cf3a3a85200f99fa61f0b4b79f9
Artyom Trityak <art.trityak@gmail.com>
2016-04-12 23:32:57 -0700

    Init get messages

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get('/webhook/', function (req, res) {
 
 
 app.post('/webhook/', function (req, res) {
+  console.log('ZZZ:', req.body);
+
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
