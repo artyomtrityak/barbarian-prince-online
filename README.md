812e9faa4f49bdd5503d5a04f5cd370056bb7cb4
Artyom Trityak <art.trityak@gmail.com>
2016-04-12 23:21:20 -0700

    Base webhook

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # forgotten-realms-mud
 Forgotten Realms MUD Node.js Facebook Messenger app
 
-# Heroku
+## Heroku
 
 Logs `heroku logs --tail`
 
 Push `git push heroku master`
 
+Detals `heroku ps`
 
+Run local `heroku local`
+
+Connect bash `heroku run bash`
+
+ENV var `heroku config:set ENV_VAR_NAME=2`, `heroku config`
+
+Database read later https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+
 app.get('/', function(request, response) {
   response.json({ok: 1});
 });
+
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_secret_webhook_change_me') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
